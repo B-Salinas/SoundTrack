663bf534f9694e02f58006bdf9a51dcd6eb60b0d
B Salinas <b.salinas397@gmail.com>
2022-03-11 21:38:42 -0600

    moved linkoverlay

--- frontend/src/components/SongCard.js
+++ frontend/src/components/SongCard.js
@@ -18,7 +18,9 @@ function SongCard({song}) {
         <Container minW='xs' borderWidth='2px' borderRadius='xl' overflow='hidden' >
           <LinkBox maxW='xs' >
             <Box mt={4}>
-              <Image w='100%' h='100%' borderRadius='xl' src={song.img_url} alt={song.song_title} />
+              <LinkOverlay href='#'>
+                <Image w='100%' h='100%' borderRadius='xl' src={song.img_url} alt={song.song_title} />
+              </LinkOverlay>
             </Box>
             <Box mt='4' mb='4' ml='1'>
               <Box color='gray.500' fontWeight='semibold' letterSpacing='wide' fontSize='xs' textTransform='uppercase' >
@@ -26,9 +28,7 @@ function SongCard({song}) {
               </Box>
               <HStack display='flex' alignItems='baseline'>
                 <Box mt='1' fontWeight='semibold' as='h4' lineHeight='tight' ml={0} mr='auto'>
-                  <LinkOverlay href='#'>
-                    {song.song_title}
-                  </LinkOverlay>
+                  {song.song_title}
                 </Box>
                 <Box mr={0} ml='auto'>
                   <LikeButton songId={song.id} />
